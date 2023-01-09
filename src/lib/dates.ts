8ae52f79967cf12acbefccf8c891d4d170e2a5c4
William Cleghorn <cleghornw@protonmail.com>
2023-01-09 13:48:22 -0700

    Added dates functionaility for copyright, and project init date

---
+++ src/lib/dates.ts
@@ -0,0 +1,12 @@
+const PROJECT_START_DATE = "2023";
+
+export const getCopyrightYear = (startYear: String): String => {
+	let currentYear = (new Date()).getFullYear();
+	// 2023 or 2023-currentYear
+	let copyRightYearRange = currentYear == startYear ?
+	"" : "-" + currentYear;
+	
+	return `© Copyright ${startYear}${copyRightYearRange}`;
+}
+
+export const getProjectCopyrightYear = () => getCopyrightYear(PROJECT_START_DATE);
