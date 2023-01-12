3e6a8649ffd72a21718130cdcda22bd5e9698458
William Cleghorn <cleghornw@protonmail.com>
2023-01-11 17:41:16 -0700

    Added deck component (WIP)

---
+++ src/lib/Deck.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Flashcard from "$lib/Flashcard.svelte";
+	import Carousel from "svelte-carousel";
+	
+	export let cards = [
+		{term: "Term", definition: "Definition"},
+//		{term: "Term", definition: "Definition"},
+//		{term: "Term", definition: "Definition"},
+//		{term: "Term", definition: "Definition"},
+	];
+	
+	let index = 0;
+</script>
+{#each cards as card, index}
+	<Flashcard 
+		term={card.term} 
+		definition={card.definition} 
+	/>
+{/each}
